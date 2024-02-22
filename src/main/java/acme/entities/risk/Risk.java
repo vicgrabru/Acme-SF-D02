da70874cca2f8e6ef5590b7ba99a9a9e883bd2e9
FranciscoRossoR <franrosora@gmail.com>
2024-02-22 22:42:29 +0100

    Task-016: Create Risk entity

---
+++ src/main/java/acme/entities/risk/Risk.java
@@ -0,0 +1,68 @@
+
+package acme.entities.risk;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Risk extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "R-[0-9]{3}")
+	private String				reference;
+
+	@NotNull
+	@Past
+	private Date				identificationDate;
+
+	@NotNull
+	@Min(0)
+	private Double				impact;
+
+	@NotNull
+	private Double				probability;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				description;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	@Transient
+	public Double value() {
+		return this.impact * this.probability;
+	}
+
+}
