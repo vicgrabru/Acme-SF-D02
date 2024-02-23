a46134f8598ce2ff47dbb1be8d4ad3dc4f6e4483
FranciscoRossoR <franrosora@gmail.com>
2024-02-23 16:16:46 +0100

    fix: impact constraints in risk entity

--- src/main/java/acme/entities/risk/Risk.java
+++ src/main/java/acme/entities/risk/Risk.java
@@ -6,11 +6,11 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -40,7 +40,7 @@ public class Risk extends AbstractEntity {
 	private Date				identificationDate;
 
 	@NotNull
-	@Min(0)
+	@Positive
 	private Double				impact;
 
 	@NotNull
