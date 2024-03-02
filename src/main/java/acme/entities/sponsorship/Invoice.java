b1ce99a2476d04b469785012e7f3baed844e9c5e
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2024-03-02 13:17:44 +0100

    Task-003-S4: create Invoices Entity

---
+++ src/main/java/acme/entities/sponsorship/Invoice.java
@@ -0,0 +1,29 @@
+
+package acme.entities.sponsorship;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invoice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	private String				code;
+
+}
