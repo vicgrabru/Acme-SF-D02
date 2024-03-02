bf92941a5ce1115761d795874b71b0e8a7b38c4b
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2024-03-02 13:21:24 +0100

    Task-003-S4: create Invoice Entity

--- src/main/java/acme/entities/sponsorship/Invoice.java
+++ src/main/java/acme/entities/sponsorship/Invoice.java
@@ -1,10 +1,21 @@
 
 package acme.entities.sponsorship;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
 import lombok.Getter;
@@ -26,4 +37,32 @@ public class Invoice extends AbstractEntity {
 	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
 	private String				code;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				registrationTime;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	private Date				dueDate;
+
+	@NotNull
+	@Positive
+	private Integer				quantity;
+
+	@NotNull
+	@PositiveOrZero
+	private Integer				tax;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	@Transient
+	public Integer totalAmount() {
+		return this.getQuantity() + this.getTax();
+	}
 }
