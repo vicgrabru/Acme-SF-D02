1c0f36a8d8ac31a6f43c104f69779a6e6c9bd56f
FranciscoRossoR <franrosora@gmail.com>
2024-03-03 14:19:38 +0100

    Fix AduitRecord entity

--- src/main/java/acme/entities/audit/AuditRecord.java
+++ src/main/java/acme/entities/audit/AuditRecord.java
@@ -1,23 +1,29 @@
 
 package acme.entities.audit;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.client.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class AuditRecord {
+public class AuditRecord extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
@@ -31,7 +37,14 @@ public class AuditRecord {
 	private String				code;
 
 	@NotNull
-	private Integer				auditPeriod;
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				periodStart;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				periodEnd;
 
 	@NotNull
 	private Mark				mark;
