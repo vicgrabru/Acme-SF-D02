f3dec2a8e39222fbcd1d7404a1ce501f76a843a8
Alberto <albescsan1@alum.us.es>
2024-03-07 18:49:57 +0100

    individual-task-s3

--- src/main/java/acme/entities/training/TrainingModule.java
+++ src/main/java/acme/entities/training/TrainingModule.java
@@ -1,70 +1,59 @@
 
-package acme.entities.objective;
+package acme.entities.training;
 
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 
+import org.checkerframework.common.aliasing.qual.Unique;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.entities.project.Project;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Objective extends AbstractEntity {
-
+public class TrainingModule extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
 	private static final long	serialVersionUID	= 1L;
 
-	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")//AAA-111, A-111
+	@Unique
+	private String				code;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	@NotNull
-	private Date				instantationMoment;
-
-	@NotBlank
-	@Length(max = 75)
-	private String				title;
+	private Date				creationMoment;
 
 	@NotBlank
 	@Length(max = 100)
-	private String				description;
+	private String				details;
 
 	@NotNull
-	private Priority			priority;
-
-	private boolean				isCritical;
+	private Difficulty			difficulty;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	private Date				startDateDuration;
+	@Past
+	private Date				updateMoment;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	private Date				endDateDuration;
+	@Temporal(TemporalType.TIME)
+	private Date				startTotalTime;
+	@Temporal(TemporalType.TIME)
+	private Date				endTotalTime;
 
 	@URL
 	private String				link;
-
-	// Relationships ----------------------------------------------------------
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	protected Project			project;
 
 }
--- src/main/java/acme/entities/training/TrainingSession.java
+++ src/main/java/acme/entities/training/TrainingSession.java
@@ -1,5 +1,5 @@
 
-package acme.entities.objective;
+package acme.entities.training;
 
 import java.util.Date;
 
@@ -8,63 +8,58 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 
+import org.checkerframework.common.aliasing.qual.Unique;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.entities.project.Project;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Objective extends AbstractEntity {
-
+public class TrainingSession extends AbstractEntity {
 	// Serialisation identifier -----------------------------------------------
 
 	private static final long	serialVersionUID	= 1L;
 
-	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@Unique
+	private String				code;
+
+	//a time period (at least one week ahead the training module creation moment, at least one week long)
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				startPeriod;
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
 	@NotNull
-	private Date				instantationMoment;
+	private Date				endPeriod;
 
 	@NotBlank
 	@Length(max = 75)
-	private String				title;
+	private String				location;
 
 	@NotBlank
-	@Length(max = 100)
-	private String				description;
+	@Length(max = 75)
+	private String				instructor;
 
 	@NotNull
-	private Priority			priority;
-
-	private boolean				isCritical;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	private Date				startDateDuration;
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@NotNull
-	private Date				endDateDuration;
+	@Email
+	private String				contactEmail;
 
 	@URL
 	private String				link;
 
-	// Relationships ----------------------------------------------------------
-
 	@NotNull
 	@Valid
 	@ManyToOne(optional = false)
-	protected Project			project;
-
+	private TrainingModule		trainingModule;
 }
