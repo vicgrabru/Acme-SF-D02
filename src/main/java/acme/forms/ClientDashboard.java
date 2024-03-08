992435e88589bc66fbbe35c66d81eb3269d1ab22
Cargarort3 <cargarort3@alum.us.es>
2024-03-08 12:10:03 +0100

    fix: Double to Money

--- src/main/java/acme/forms/ClientDashboard.java
+++ src/main/java/acme/forms/ClientDashboard.java
@@ -2,6 +2,7 @@
 package acme.forms;
 
 import acme.client.data.AbstractForm;
+import acme.client.data.datatypes.Money;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,9 +21,9 @@ public class ClientDashboard extends AbstractForm {
 	private Integer				between50and75CompletenessProgressLogs;
 	private Integer				above75CompletenessProgressLogs;
 
-	private Double				avgContractBudget;
-	private Double				stdContractBudget;
-	private Double				minContractBudget;
-	private Double				maxContractBudget;
+	private Money				avgContractBudget;
+	private Money				stdContractBudget;
+	private Money				minContractBudget;
+	private Money				maxContractBudget;
 
 }
